756e0ecf747e834f3484270e56ce80281a533df1
intisarraed <intisarraed123@gmail.comgit config --global user.name>
2020-11-15 20:28:50 +0200

    adding script for adding topic

--- adminPanel/script.js
+++ adminPanel/script.js
@@ -1,26 +1,59 @@
    let input = document.getElementById('topic')
    let form = document.querySelector('form')
-       let itemsArray = localStorage.getItem('topic')
-    ? JSON.parse(localStorage.getItem('topic'))
-    : [];
+   let button = document.getElementById('saveTopic')
+    let itemsArray = localStorage.getItem('topic') ? JSON.parse(localStorage.getItem('topic')) : [];
     localStorage.setItem('topic', JSON.stringify(itemsArray))
-    const data = JSON.parse(localStorage.getItem('topic'))
-    const liMaker = (text) => {
-    const table = document.createElement('table')
-    const tr = document.createElement("tr");
-    const td = document.createElement("td");
-    td.textContent = text
-    tr.appendChild(td);
-    table.appendChild(tr);
-    }
+    // const liMaker = (text) => {
+    // const table = document.createElement('table')
+    // const tr = document.createElement("tr");
+    // const td = document.createElement("td");
+    // td.textContent = text
+    // tr.appendChild(td);
+    // table.appendChild(tr);
+    // }
 
     button.addEventListener('click', function (e) {
         e.preventDefault()
         itemsArray.push(topic.value)
         localStorage.setItem('topic', JSON.stringify(itemsArray))
-        liMaker(topic.value)
+        // liMaker(topic.value)
         topic.value = ''
-        data.forEach((topic) => {
-        liMaker(topic)
-        })
+        // data.forEach((topic) => {
+        // liMaker(topic)
+        // })
     })
+    console.log(itemsArray)
+
+
+// let form = document.querySelector('form')
+// let input = document.getElementById("title");
+// let itemsArray = localStorage.getItem('topic') ? JSON.parse(localStorage.getItem('topic')) : []
+//     console . log(itemsArray)
+// localStorage.setItem('topic', JSON.stringify(itemsArray))
+// const data = JSON.parse(localStorage.getItem('topic'))
+// from.addEventListener('submit', function (e) {
+//     e.preventDefault()
+//     itemsArray.push(topic.value)
+//     localStorage.setItem('topic', JSON.stringify(itemsArray))
+//     input.value = ''
+// })
+// console . log(itemsArray)
+//     const liMaker = (text) => {
+//     const table = document.createElement('table')
+//     const tr = document.createElement("tr");
+//     const td = document.createElement("td");
+//     td.textContent = text
+//     tr.appendChild(td);
+//     table.appendChild(tr);
+//     }
+
+//     button.addEventListener('click', function (e) {
+//         e.preventDefault()
+//         itemsArray.push(topic.value)
+//         localStorage.setItem('topic', JSON.stringify(itemsArray))
+//         liMaker(topic.value)
+//         topic.value = ''
+//         data.forEach((topic) => {
+//         liMaker(topic)
+//         })
+//     })
---
+++ adminPanel/viewtopicscript.js
@@ -0,0 +1,15 @@
+document.getElementById("topicData").innerHTML = localStorage.getItem("topic");
+
+// const data = JSON.parse(localStorage.getItem('topic'))
+// const topic = document.getElementById()
+// data.forEach((topic) => {
+//     liMaker(topic)
+//     })
+// const liMaker = (text) => {
+//     const table = document.createElement('table')
+//     const tr = document.createElement("tr");
+//     const td = document.createElement("td");
+//     td.textContent = text
+//     tr.appendChild(td);
+//     table.appendChild(tr);
+//     }
